508198cac569bcdd4e28ddaf5f243e1adbf97f86
Abh2090 <108894666+Abh2090@users.noreply.github.com>
2026-03-30 01:54:55 +0530

    Add test message for auto trigger functionality

--- app.sh
+++ app.sh
@@ -10,3 +10,4 @@ ls -l
 echo " auto trigger working after uploading test file named app.sh "
 echo "✅ Build Successful!"
 echo " latest auto trigger working successfully named app.sh !!"
+echo " testing this auto trigger after watching kapil sharma show ! "

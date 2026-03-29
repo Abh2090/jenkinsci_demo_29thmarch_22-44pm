711b065159198720c045e4ca380c4963cb20ff7e
Abh2090 <108894666+Abh2090@users.noreply.github.com>
2026-03-30 01:53:05 +0530

    Update success message in app.sh

--- app.sh
+++ app.sh
@@ -9,4 +9,4 @@ echo "📂 Listing files:"
 ls -l
 echo " auto trigger working after uploading test file named app.sh "
 echo "✅ Build Successful!"
-echo " latest auto trigger working successfully named app.sh"
+echo " latest auto trigger working successfully named app.sh !!"

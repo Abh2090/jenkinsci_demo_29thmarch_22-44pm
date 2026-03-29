8bca325f3a1ff620e501fdc41bce8e894d3ce07a
Abh2090 <108894666+Abh2090@users.noreply.github.com>
2026-03-30 01:59:21 +0530

    Fix typo in echo statement in app.sh

--- app.sh
+++ app.sh
@@ -10,4 +10,4 @@ ls -l
 echo " auto trigger working after uploading test file named app.sh "
 echo "✅ Build Successful!"
 echo " latest auto trigger working successfully named app.sh !!"
-echo " testing this auto trigger after watching kapil sharma show ! "
+echo " testing this auto trigger after watching the  kapil sharma show ! "
